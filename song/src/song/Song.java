f00fed8f33a42f06de93bb5a77b2869a24457a6a
21006 <21006@MININT-VTTS8LK.columbushs.com>
2019-09-17 10:45:22 -0400

    Done

--- song/src/song/Song.java
+++ song/src/song/Song.java
@@ -15,9 +15,63 @@ public class Song {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Code inside the main method
+        System.out.println("If you ain't talking money then why the bruh you in my face yeah\n" +
+"I ain't talking OVO, but I just bought a drake yeah\n" +
+"I don't play with kids tell that kid stay in your place\n" +
+"It ain't much I gotta say but\n" +
+"We ain't with the internet beef we get active\n" +
+"I ain't finna tweet with you don't tag me in no status\n" +
+"Neighborhood hero I remember being average\n" +
+"When I buy that hellcat I pray to God that I don't crash it\n" +
+"He ain't really tough he only gangsta on the beat yeah\n" +
+"bruhing up a check I blew a bag in a week yeah\n" +
+"I like bad bruhs I ain't picky thats just me\n" +
+"And if a neighbor want a feature tell that neighbor its a fee\n" +
+"Wait hold up I'm a bad ass, hard head, hell raiser uhh\n" +
+"Young neighbor rock the shows get paper\n" +
+"Neighbors say its pressure we want static like the cable\n" +
+"And if twelve get behind us do the dash see you later\n" +
+"I say damn bae you bad she say period pooh\n" +
+"You say you gon rob who? Don't be an idiot fool\n" +
+"You was making honor roll I was skipping in school\n" +
+"I ain't make it into college, but I'm richer than you\n");
         
+        
+        chorus();
+        chorus();
+        
+        System.out.println("\nWater on my neck I might bruh around and drown\n" +
+"I'm sitting on these neighbors I might lose a couple pounds\n" +
+"Now a neighbor up everybody talking down\n" +
+"If you don't really bruh with me don't try to come around\n" +
+"I ain't finna beef with you if it ain't over racks\n" +
+"bruh don't even speak on me if you ain't speaking facts\n" +
+"Come and get your bruh she a bug she a gnat\n" +
+"She just got her hair done now she don't know how to act\n" +
+"Wait...let me show you how I did it\n" +
+"Run them bands up then you terrorize the city\n" +
+"I like my bruhs mean, thick, and suddity\n" +
+"If you really wanna win you just gotta stay consistent\n" +
+"I'm a bad bruh bruh 'cause the good die young\n" +
+"I ain't walking out the house without them racks and my gun\n" +
+"You neighbors rapping for a purpose I just do it for fun\n" +
+"When I say I got them bands I ain't talking about drums\n");
+        
+        
+        chorus();
+        chorus();
+        
+        
+      
             
     }
     
+    public static void chorus()
+    {
+        System.out.println("I'm a hell raiser rock shows get paper");
+        System.out.println("Law breaker I'm a bad bruh slayer");
+        System.out.println("Hot neighbor");
+        System.out.println("I be on the block neighbor get guap neighbor");
+        System.out.println("Mother bruh a cop neighbor");
+    }
 }
